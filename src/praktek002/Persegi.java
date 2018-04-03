b0a89e1d933cc255aaeabfce1217bd0c598fd5fb
Khairul Ikhsan Nurzeha <32305997+ikhsannz@users.noreply.github.com>
2018-04-03 17:56:12 +0800

    Membuat Attribut dan Behavior

--- src/praktek002/Persegi.java
+++ src/praktek002/Persegi.java
@@ -3,5 +3,13 @@ package praktek002;
 
 
 public class Persegi {
+    double sisi;
+    
+    
+    void cetakInfo(){
+        System.out.println("+==================+");
+        System.out.println("sisi     :"+sisi);
+        System.out.println("+==================+");
+    }
     
 }
